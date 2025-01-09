fecd707479a5df9fefe4d57cc63366184a9d9511
RezaPahlevi01 <akhmadretzasyahpahlevi@gmail.com>
2025-01-09 10:35:02 +0700

    update db.js

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     host: process.env.DB_HOST || 'homelaundry.my.id',
     username: process.env.DB_USER || 'homelaun_capstone_ulhaq',
     password: process.env.DB_PASSWORD || 'capstone_ulhaq_1234_',
